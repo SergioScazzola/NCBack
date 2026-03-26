3cb236c91dae2f70a80ea927ea360c6689a3644a
Sergio Scazzola <scazzolasergio@gmail.com>
2026-03-25 23:45:42 -0300

    update front

--- FrontEnd/LogFront/src/app/app.config.ts
+++ FrontEnd/LogFront/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { withInterceptors, provideHttpClient } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor'; // AJUSTA LA RUTA SEGÚN TU PROYECTO
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    // Registramos el interceptor funcional aquí:
+    provideHttpClient(
+      withInterceptors([authInterceptor]) 
+    )
   ]
 };
